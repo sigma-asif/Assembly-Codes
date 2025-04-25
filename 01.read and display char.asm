ed726f58a77ae7663c2d57a04afb2f511334d8b3
Asif Rahman <119958726+sigma-asif@users.noreply.github.com>
2025-04-25 20:56:17 +0600

    Add files via upload

---
+++ 01.read and display char.asm
@@ -0,0 +1,28 @@
+.model small
+.stack size 100h 
+.data
+.code
+
+main proc 
+    mov ax, @data
+    mov ds, ax
+    
+    ; read char
+    mov ah, 1; dos funtion to read char
+    int 21h 
+    mov bl, al; 
+    
+    
+    ;display char
+    mov ah, 2
+    mov dl, bl
+    int 21h
+    
+    
+    
+    
+    ;exit
+    mov ah, 4ch
+    int 21h
+    
+end main    
